9cbd3814f1bdc70a80886d297dc79a2d42a3bcf3
Su-Ann <162672107+slam0051@users.noreply.github.com>
2024-07-10 23:13:57 +1000

    Update quizjav.js
    so total score revealed at the end of quiz - neatness and conventions of code still need to be fixed though

--- quizjav.js
+++ quizjav.js
@@ -1,122 +1,4 @@
-// $(document).ready(function(){
 
-//     var questions = [{
-//         text:"do trees exist",
-//         type: "truefalse",
-//         answer: true},
-
-//         {
-//         text:"name",
-//     
-//         choices: ["ad", "zdfg", "aegdafg", "ga"],
-//         answer: "ad"},
-
-//         {text:"Age",
-//     
-//         choices: ["ad", "asssssf", "dfg", "ga"],
-//         answer: "ad"},
-        
-//         {text:"height",
-//     
-//         choices: ["ad", "af", "dfg", "ga"],
-//         answer: "ad"},
-
-//         {text:"pot",
-//     
-//         choices: ["ad", "af", "dfg", "ga"],
-//         answer: "ad"}
-
-//     ];
-
-//     // intialising question 
-//     var currentquestion = 0
-//     $('#question').text("Question " + (currentquestion+ 1) + ": " + questions[currentquestion].text);
-
-//     // truefalse question type 
-//     function truefalsechecking(){
-//         // $("center-buttons").empty();
-//         // $(".center-buttons").append((<button type="button" class="btn btn-primary">True</button>), (<button type="button" class="btn btn-secondary">False</button>));
-
-//         // if correct answer chosen
-//         $(".btn-primary").click(function(){
-//             if ($(this).text() === "True" && questions[currentquestion].answer === true||
-//             $(this).text() === "False" && questions[currentquestion].answer === false)
-//                 {$(".btn-primary").addClass("btn btn-success");
-//                 alert("Correct answer!");}
-
-//             else{
-//                 $(".btn-primary").addClass("btn btn-danger");
-//                 alert("Wrong answer, please try again!");}
-//         });
-
-//         $(".btn-secondary").click(function(){
-//             if ($(this).text() === "True" && questions[currentquestion].answer === true||
-//             $(this).text() === "False" && questions[currentquestion].answer === false)
-//             {$(".btn-secondary").addClass("btn btn-success");
-//             alert("Correct answer!");}
-
-//             else{
-//                 $(".btn-secondary").addClass("btn btn-danger");
-//                 alert("Wrong answer, please try again!");}
-        
-//         });
-//     }
-
-//     // multichoice question
-//     function multichoicechecking() { 
-//         for (var i = 0; i < questions[currentquestion].choices.length; i++) {
-
-//             (function(index) {
-//                 var choiceButton = $('<button type="button" class="btn btn-primary">' + questions[currentquestion].choices[index] + '</button>');
-//                 $(".center-buttons").append(choiceButton);
-//                 choiceButton.click(function() {
-//                     if ($(this).text() === questions[currentquestion].answer) {
-//                         $(this).addClass("btn btn-success");
-//                         alert("Correct answer!");
-//                     } else {
-//                         $(this).addClass("btn btn-danger");
-//                         alert("Wrong answer, please try again!");
-//                     }
-//                 });
-
-//             })(i);
-//         }
-//     }
-    
-//     // what type the question is 
-    
-//     function typeofquestion(){
-//         $(".center-buttons").empty();
-//         $('#question').text("Question " + (currentquestion+ 1) + ": " + questions[currentquestion].text);
-
-//         if (questions[currentquestion].type === "truefalse") {
-//             truefalsechecking();
-//         }
-//         else if (questions[currentquestion].type === "multichoice") {
-//             multichoicechecking();
-//         }
-//     }
-//     // typeofquestion()
-
-//     // NEXT BUTTON IMPLEMENTATION
-//     $("#nextbutton").click(function(){
-//         // removing the colours that indicate the right and wrong answer
-//         $(".btn-primary").removeClass("btn-success btn-danger");
-//         $(".btn-secondary").removeClass("btn-success btn-danger");
-
-//         // moving onto the next question
-//         currentquestion++;
-//         if (currentquestion < questions.length){
-
-//             typeofquestion()}
-            
-//         else{
-//             alert("THE END - do a print statement instead here");}
-//         });
-
-        
-   
-// })
 
 $(document).ready(function(){
 
@@ -157,8 +39,13 @@ $(document).ready(function(){
     function multichoicechecking() { 
         for (var i = 0; i < questions[currentquestion].choices.length; i++) {
 
+<<<<<<< HEAD
             // (function(index) {
                 var choiceButton = $('<button type="button" class="btn btn-primary">' + questions[currentquestion].choices[i] + '</button>');
+=======
+            (function(index) {
+                var choiceButton = $('<button type="button" class="btn btn-primary">' + questions[currentquestion].choices[index] + '</button>');
+>>>>>>> 99eacdd05fdba6bcdcbc0d45d4f17e6fa470c0e8
                 $(".center-buttons").append(choiceButton);
         var p = 0
         choiceButton.click(function() {
@@ -177,14 +64,10 @@ $(document).ready(function(){
             value --
             p --
         }
-                        
-                    // } else {
-                    //     $(this).addClass("btn btn-danger");
-                    //     alert("Wrong answer, please try again!");
-                    // }
+               
                 });
 
-            // })(i);
+            })(i);
         }
     }
     
@@ -217,31 +100,3 @@ $(document).ready(function(){
 })
 
 
-// id unique for the page - sigle element 
-// class multiple 
-
-
-// quiz - user experience  - try to pick any questions and then only tell you if right or wrong if oce you have
-
-// camel case, capital letter for each word currentQuestion
-// render question - instead of type of question
-// change truefalse to be like multiple choice instead 
-
-
-// choices:
-// {text:"true", correct: true, ...
-// }
-// ||
-// right answer: adijfg
-// wrong answer =[afag,gaf,dgf]
-
-// function correctanswer(input)
-
-// questions.forEach((questions))
-//     question.choices.forEach
-//         if (choice === input){
-//             return true;}
-        
-
-
-//             console.log{"correct" +}
