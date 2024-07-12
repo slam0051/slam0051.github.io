9b534f1f01f60574e450ac3fdc44890ccf57d114
Su-Ann <162672107+slam0051@users.noreply.github.com>
2024-07-13 00:50:36 +1000

    updating quiz js
    making the code more consistent and neater

--- quizjav.js
+++ quizjav.js
@@ -9,63 +9,74 @@ $(document).ready(function(){
         {
         text:"which of the following are a renewable energy source",
         choices: ["coal", "natural gas", "solar", "nuclear"],
-        answer: "solar"},
+        answer: "solar"
+        },
 
-        {text:"Which of these activities contributes most to deforestation?",
+        {
+        text:"Which of these activities contributes most to deforestation?",
         choices: ["Sustainable logging", "Urbanization", "Agriculture", "Wildfires"],
-        answer: "Agriculture"},
+        answer: "Agriculture"
+        },
         
-        {text:"What is the primary purpose of a carbon footprint?",
+        {
+        text:"What is the primary purpose of a carbon footprint?",
         choices: ["Measure the amount of carbon dioxide in the atmosphere", "Assess the impact of individual or organizational activities on climate change", "Calculate the financial cost of carbon emissions", "Determine the efficiency of renewable energy sources"],
-        answer: "Assess the impact of individual or organizational activities on climate change"},
+        answer: "Assess the impact of individual or organizational activities on climate change"
+        },
 
-        {text:"Water scarcity is primarily caused by natural factors and is not influenced by human activities.",
+        {
+        text:"Water scarcity is primarily caused by natural factors and is not influenced by human activities.",
         choices: ["True", "False"],
-        answer: "False"},
+        answer: "False"
+        },
 
-        {text:"what colour is grass",
+        {
+        text:"what colour is grass",
         choices: ["green", "blue", "red", "hot neon pink"],
-        answer: "green"}
-
+        answer: "green"
+        }
     ];
 
-    // intialising question and total score
-    var currentquestion = 0
+
+    // intialising question and total score system
+    var currentQuestion = 0
     var value = 0
-    $('#question').text("Question " + (currentquestion+ 1) + ": " + questions[currentquestion].text);
+    $('#question').text("Question " + (currentQuestion+ 1) + ": " + questions[currentQuestion].text);
 
     // multichoice question
-    function multichoicechecking() { 
-        for (var i = 0; i < questions[currentquestion].choices.length; i++) {
-
-            var choiceButton = $('<button type="button" class="btn btn-primary">' + questions[currentquestion].choices[i] + '</button>');
+    function multiChoice() { 
+        for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+            var choiceButton = $('<button type="button" class="btn btn-primary">' + questions[currentQuestion].choices[i] + '</button>');
             $(".center-buttons").append(choiceButton);
-        var p = 0
-        choiceButton.click(function() {
-        $(".btn-primary").removeClass("btn-success");
-        $(".btn-secondary").removeClass("btn-success");
-        $(this).addClass("btn btn-success");
-    
-        if ($(this).text() === questions[currentquestion].answer) {
-            if (p === 0){
-            value ++
-            p ++    }
-        }
 
-        if (p===1 && $(this).text() !== questions[currentquestion].answer) {
-            value --
-            p --}   
-        })
-    }}
+            var p = 0
+            choiceButton.click(function() {
+            $(".btn-primary").removeClass("btn-success");
+            $(".btn-secondary").removeClass("btn-success");
+            $(this).addClass("btn btn-success");
+        
+            if ($(this).text() === questions[currentQuestion].answer) {
+                if (p === 0){
+                value ++;
+                p ++
+            ;}}
+
+            if (p===1 && $(this).text() !== questions[currentQuestion].answer) {
+                value --;
+                p --
+            ;}   
+            })
+        }
+    }
     
     // what type the question is 
     
-    function typeofquestion(){
+    function renderQuestion(){
         $(".center-buttons").empty();
-        $('#question').text("Question " + (currentquestion+ 1) + ": " + questions[currentquestion].text);
-        multichoicechecking();}
+        $('#question').text("Question " + (currentQuestion+ 1) + ": " + questions[currentQuestion].text);
+        multiChoice();}
         
-    typeofquestion()
+    renderQuestion();
 
     // NEXT BUTTON IMPLEMENTATION
     $("#nextbutton").click(function(){
@@ -74,15 +85,11 @@ $(document).ready(function(){
         $(".btn-secondary").removeClass("btn-success btn-danger");
 
         // moving onto the next question
-        currentquestion++;
-        if (currentquestion < questions.length){
-            typeofquestion()}
+        currentQuestion++;
+        if (currentQuestion < questions.length){
+            renderQuestion();}
             
         else{
             alert("THE END. You scored: " + value + "/6")}
-        });        
-   
-    // total value score gotten
-    console.log("correct: " + value);
+        });         
 })
-
